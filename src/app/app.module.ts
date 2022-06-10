2f743762004ca78d2927c417787d7a99ee9ea1cc
suhasballal333 <rsuhasballal@gmail.com>
2022-06-10 16:50:20 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,30 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { HttpModule } from "@angular/http";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import { AppComponent } from "./app.component";
-import { NavbarComponent } from "./navbar/navbar.component";
-import { DemoComponent } from "./demo/demo.component";
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DemoComponent } from './demo/demo.component';
 
-import { SocialLoginModule } from "angularx-social-login";
+import { SocialLoginModule } from 'angularx-social-login';
 import {
   AuthServiceConfig,
   GoogleLoginProvider,
-  FacebookLoginProvider
-} from "angularx-social-login";
+  FacebookLoginProvider,
+} from 'angularx-social-login';
 
 const config = new AuthServiceConfig([
   {
     id: GoogleLoginProvider.PROVIDER_ID,
     provider: new GoogleLoginProvider(
-      "669942312628-q11p4ebfgujdk5bmbd66n8bl2a5ppt6q.apps.googleusercontent.com"
-    )
+      '455606496481-atmheq2usmemkucr9lievof2bb2qs0f7.apps.googleusercontent.com'
+    ),
   },
   {
     id: FacebookLoginProvider.PROVIDER_ID,
-    provider: new FacebookLoginProvider("229459261889027")
-  }
+    provider: new FacebookLoginProvider('229459261889027'),
+  },
 ]);
 
 export function provideConfig() {
@@ -37,9 +37,9 @@ export function provideConfig() {
   providers: [
     {
       provide: AuthServiceConfig,
-      useFactory: provideConfig
-    }
+      useFactory: provideConfig,
+    },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
